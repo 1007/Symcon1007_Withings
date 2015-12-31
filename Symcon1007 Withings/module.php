69dbb0ab8cff4dc82791016aa01c85025e4b4ad1
1007 <juergen.gerharz@inisnet.de>
2015-12-31 11:00:11 +0100

    Init

--- Symcon1007 Withings/module.php
+++ Symcon1007 Withings/module.php
@@ -2,19 +2,63 @@
 	class Withings extends IPSModule
 	{
 		
+
+
+    //**************************************************************************
+    //
+    //**************************************************************************    
+    public function Create()
+      {
+      //Never delete this line!
+      parent::Create();
+    
+      $this->RegisterPropertyInteger("Intervall", 21600);  
+        
+      }
+    
+
+    //**************************************************************************
+    //
+    //**************************************************************************    
 		public function ApplyChanges()
-		{
+		  {
 			//Never delete this line!
 			parent::ApplyChanges();
 			
 			//Lets register a variable with action
 			$this->RegisterVariableInteger("Withings", "Test", "~Intensity.100");
 			$this->EnableAction("Withings");
-			
-		}
-		
+      $this->RegisterPropertyInteger("Intervall", 21600);
+      //$this->RegisterTimer("WIT_UpdateTimer", 0, 'WIT_Update($_IPS[\'TARGET\']);');
+
+	    //Timer erstellen
+      $this->SetTimerInterval("WIT_UpdateTimer", $this->ReadPropertyInteger("Intervall"));
+ 
+
+      //Update
+     	$this->Update();
+
+		  }
+
+    //**************************************************************************
+    //
+    //**************************************************************************    
+    public function Destroy()
+      {
+      $this->UnregisterTimer("WIT_UpdateTimer");
+        
+      //Never delete this line!
+      parent::Destroy();
+      }
+
+
+
+
+    //**************************************************************************
+    //
+    //**************************************************************************    
 		public function RequestAction($Ident, $Value)
-		{
+		  {
 			
 			switch($Ident) {
 				case "Withings":
@@ -24,7 +68,7 @@
 					throw new Exception("Invalid ident");
 			}
 		
-		}
+		  }
 		
 	
 	}
