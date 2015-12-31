3e1f9aec3f57081b218a0d875f78f06a33029748
1007 <juergen.gerharz@inisnet.de>
2015-12-31 16:31:46 +0100

    -

--- Symcon1007 Withings/module.php
+++ Symcon1007 Withings/module.php
@@ -13,6 +13,15 @@
       parent::Create();
     
       $this->RegisterPropertyInteger("Intervall", 21600);  
+      $this->RegisterPropertyBoolean("BodyMeasures", true);  
+      $this->RegisterPropertyBoolean("ActivityMeasures", false);  
+      $this->RegisterPropertyBoolean("IntradayActivity", false);  
+      $this->RegisterPropertyBoolean("SleepMeasures", false);  
+      $this->RegisterPropertyBoolean("SleepSummary", false);  
+      $this->RegisterPropertyString("Username", "user@user.de");  
+      $this->RegisterPropertyString("Userpassword", "123456");  
+      $this->RegisterPropertyString("User", "XXX");  
+      $this->RegisterPropertyBoolean("Logging", false);  
       $this->RegisterTimer("WIT_UpdateTimer", 0, 'WIT_Update($_IPS[\'TARGET\']);');
         
       }
@@ -30,14 +39,11 @@
 			$this->RegisterVariableInteger("Withings", "Test", "~Intensity.100");
 			$this->EnableAction("Withings");
       
-
 	    //Timer erstellen
       $this->SetTimerInterval("WIT_UpdateTimer", $this->ReadPropertyInteger("Intervall"));
  
-
       //Update
      	$this->Update();
-
 		  }
 
     //**************************************************************************
@@ -71,83 +77,86 @@
 		public function RequestAction($Ident, $Value)
 		  {
 			
-			switch($Ident) {
+			switch($Ident) 
+        {
 				case "Withings":
 					SetValue($this->GetIDForIdent($Ident), $Value);
 					break;
 				default:
 					throw new Exception("Invalid ident");
-			}
+			   }
 		
 		  }
 		
 
-   protected function RegisterTimer($Name, $Interval, $Script)
-    {
-        $id = @IPS_GetObjectIDByIdent($Name, $this->InstanceID);
-        if ($id === false)
-            $id = 0;
-        if ($id > 0)
+    protected function RegisterTimer($Name, $Interval, $Script)
+      {
+      $id = @IPS_GetObjectIDByIdent($Name, $this->InstanceID);
+      if ($id === false)
+        $id = 0;
+      if ($id > 0)
         {
-            if (!IPS_EventExists($id))
-                throw new Exception("Ident with name " . $Name . " is used for wrong object type", E_USER_WARNING);
-            if (IPS_GetEvent($id)['EventType'] <> 1)
+          if (!IPS_EventExists($id))
+            throw new Exception("Ident with name " . $Name . " is used for wrong object type", E_USER_WARNING);
+          if (IPS_GetEvent($id)['EventType'] <> 1)
             {
-                IPS_DeleteEvent($id);
-                $id = 0;
+            IPS_DeleteEvent($id);
+            $id = 0;
             }
         }
-        if ($id == 0)
+      if ($id == 0)
         {
-            $id = IPS_CreateEvent(1);
-            IPS_SetParent($id, $this->InstanceID);
-            IPS_SetIdent($id, $Name);
+        $id = IPS_CreateEvent(1);
+        IPS_SetParent($id, $this->InstanceID);
+        IPS_SetIdent($id, $Name);
         }
-        IPS_SetName($id, $Name);
-        IPS_SetHidden($id, true);
-        IPS_SetEventScript($id, $Script);
-        if ($Interval > 0)
+      IPS_SetName($id, $Name);
+      IPS_SetHidden($id, true);
+      IPS_SetEventScript($id, $Script);
+      if ($Interval > 0)
         {
-            IPS_SetEventCyclic($id, 0, 0, 0, 0, 1, $Interval);
-            IPS_SetEventActive($id, true);
-        } else
+        IPS_SetEventCyclic($id, 0, 0, 0, 0, 1, $Interval);
+        IPS_SetEventActive($id, true);
+        } 
+      else
         {
-            IPS_SetEventCyclic($id, 0, 0, 0, 0, 1, 1);
-            IPS_SetEventActive($id, false);
+        IPS_SetEventCyclic($id, 0, 0, 0, 0, 1, 1);
+        IPS_SetEventActive($id, false);
         }
-    }
+      }
+      
     protected function UnregisterTimer($Name)
-    {
-        $id = @IPS_GetObjectIDByIdent($Name, $this->InstanceID);
-        if ($id > 0)
+      {
+      $id = @IPS_GetObjectIDByIdent($Name, $this->InstanceID);
+      if ($id > 0)
         {
-            if (!IPS_EventExists($id))
-                throw new Exception('Timer not present', E_USER_NOTICE);
-            IPS_DeleteEvent($id);
+        if (!IPS_EventExists($id))
+          throw new Exception('Timer not present', E_USER_NOTICE);
+        IPS_DeleteEvent($id);
         }
-    }
+      }
     
     protected function SetTimerInterval($Name, $Interval)
-    {
-        $id = @IPS_GetObjectIDByIdent($Name, $this->InstanceID);
-        if ($id === false)
-            throw new Exception('Timer not present', E_USER_WARNING);
-        if (!IPS_EventExists($id))
-            throw new Exception('Timer not present', E_USER_WARNING);
-        $Event = IPS_GetEvent($id);
-        if ($Interval < 1)
+      {
+      $id = @IPS_GetObjectIDByIdent($Name, $this->InstanceID);
+      if ($id === false)
+        throw new Exception('Timer not present', E_USER_WARNING);
+      if (!IPS_EventExists($id))
+        throw new Exception('Timer not present', E_USER_WARNING);
+      $Event = IPS_GetEvent($id);
+      if ($Interval < 1)
         {
-            if ($Event['EventActive'])
-                IPS_SetEventActive($id, false);
+        if ($Event['EventActive'])
+          IPS_SetEventActive($id, false);
         }
-        else
+      else
         {
-            if ($Event['CyclicTimeValue'] <> $Interval)
-                IPS_SetEventCyclic($id, 0, 0, 0, 0, 1, $Interval);
-            if (!$Event['EventActive'])
-                IPS_SetEventActive($id, true);
+        if ($Event['CyclicTimeValue'] <> $Interval)
+          IPS_SetEventCyclic($id, 0, 0, 0, 0, 1, $Interval);
+        if (!$Event['EventActive'])
+          IPS_SetEventActive($id, true);
         }
-    }
+      }
 	
 	}
 ?>
